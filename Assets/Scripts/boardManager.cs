66380be48758725808ce64548f6cc2ed114b5f68
emmapy <emmapyates@gmail.com>
2024-02-26 14:36:19 -0800

    create board and boardState with rudimentary piece placing

---
+++ Assets/Scripts/boardManager.cs
@@ -0,0 +1,106 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Image = UnityEngine.UI.Image;
+using Vector3 = UnityEngine.Vector3;
+
+public class boardManager : MonoBehaviour
+{
+
+    public int boardSize;
+    public GameObject testPiece;
+
+    RectTransform rectT;
+
+    string[,] boardState;
+
+    void Awake() {
+        if (boardSize == 0) {
+            Debug.LogError("Initial board size is invalid.  Exit");
+            return;
+        }
+        
+
+        // create chess board
+        createBoard(boardSize);
+    
+    }
+    // Start is called before the first frame update
+    void Start() {
+        placeItem(testPiece, 4, 4);
+        printBoardState();
+    }
+
+    void createBoard(int size) {
+        rectT = GetComponent<RectTransform>();
+        Graphic G = GetComponent<Graphic>();
+        Material mat = G.material;
+        mat.SetFloat("_boardDim", boardSize);
+
+        initBoardState();
+
+    }
+
+    void initBoardState() {
+        boardState = new string[boardSize, boardSize];
+        for (int i = 0; i < boardState.GetLength(0); i++) {
+            for (int j = 0; j < boardState.GetLength(1); j++) {
+                boardState[i, j] = "-";
+            }
+        }
+    }
+
+    void printBoardState() {
+        string toPrint = "PRINTED BOARD =>";
+
+        string col = "";
+        
+        for (int j = 0; j<boardState.GetLength(1); j++){
+            col += "\tc" + j;
+        }
+        toPrint += "\n" + col;
+
+        for (int i = 0; i < boardState.GetLength(0); i++) {
+            string row = "r"+i;
+
+            for (int j = 0; j < boardState.GetLength(1); j++) {
+                row += "\t" + boardState[i, j] + " ";
+            }
+            toPrint += "\n" + row;
+        }
+
+        Debug.Log(toPrint);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void placeItem(GameObject obj, int x, int y) {
+        // check for user errors...
+        Debug.Assert(
+            (x > 0) && (x < boardSize) && (y > 0) && (y < boardSize), 
+            "The input coordinate lies outside the board's dimensions."
+        );
+
+        Debug.Assert(rectT.pivot == new Vector2(0, 1), "The board's pivot is not 0,1. Fix in inspector");
+
+        Vector3[] corners = new Vector3[4];
+        rectT.GetWorldCorners(corners);
+
+        // world width and height
+        float wWidth = Math.Abs(corners[0].x - corners[2].x);
+        float wHeight = Math.Abs(corners[0].y - corners[1].y);
+
+        Vector2 cellSz = new Vector2(wWidth / boardSize, wHeight / boardSize);
+        Vector3 boardLoc = new Vector3(x * cellSz.x + cellSz.x/2, -1 * (y * cellSz.y + cellSz.y/2), 0);
+        
+        obj.transform.position = rectT.position + boardLoc;
+        boardState[x, y] = "X";
+
+
+    }
+}
